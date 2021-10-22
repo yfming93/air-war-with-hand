208a8fdd0894379511a7046c20d4249e954c16b8
davie <iscooleye@163.com>
2021-10-22 16:08:03 +0800

    update

--- js/hand.js
+++ js/hand.js
@@ -11,11 +11,11 @@ function onResults(results) {
   // Hide the spinner.
   document.body.classList.add('loaded');
 
-  // Draw the overlays.
-//   canvasCtx.save();
-//   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-//   canvasCtx.drawImage(
-//     results.image, 0, 0, canvasElement.width, canvasElement.height);
+  // 绘制遮罩
+  // canvasCtx.save();
+  // canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  // canvasCtx.drawImage(
+  //   results.image, 0, 0, canvasElement.width, canvasElement.height);
 
   if (results.multiHandLandmarks && results.multiHandedness) {
     for (let index = 0; index < results.multiHandLandmarks.length; index++) {
@@ -23,25 +23,26 @@ function onResults(results) {
       const isRightHand = classification.label === 'Right';
       const landmarks = results.multiHandLandmarks[index];
 
-      let x = landmarks[8].x * 1280;
-      let y = landmarks[8].y * 720;
-    //   hero.style.left = x - 50 + 'px';
-    //   hero.style.top = y - 40 + 'px';
-    movePlaneWithFinger(x,y)
+    // 通过手势移动飞机，把手势坐标传给飞机
+    movePlaneWithFinger(landmarks[8].x,landmarks[8].y)
 
-    //   drawConnectors(
-    //     canvasCtx, landmarks, HAND_CONNECTIONS,
-    //     { color: isRightHand ? '#00FF00' : '#FF0000' }),
-    //     drawLandmarks(canvasCtx, landmarks, {
-    //       color: isRightHand ? '#00FF00' : '#FF0000',
-    //       fillColor: isRightHand ? '#FF0000' : '#00FF00',
-    //       radius: (x) => {
-    //         return lerp(x.from.z, -0.15, .1, 10, 1);
-    //       }
-    //     });
+    // 绘制手部拓扑
+      // drawConnectors(
+      //   canvasCtx, landmarks, HAND_CONNECTIONS,
+      //   { color: isRightHand ? '#00FF00' : '#FF0000' }),
+      //   drawLandmarks(canvasCtx, landmarks, {
+      //     color: isRightHand ? '#00FF00' : '#FF0000',
+      //     fillColor: isRightHand ? '#FF0000' : '#00FF00',
+      //     radius: (x) => {
+      //       return lerp(x.from.z, -0.15, .1, 10, 1);
+      //     }
+      //   });
+
     }
   }
-//   canvasCtx.restore();
+
+  // canvasCtx.restore();
+
 }
 
 const hands = new Hands({
